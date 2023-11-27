8ab8c5abf9665a4476d51e2b75b1d07ef453f73f
Allen-Josu <allenalackaparambil@gmail.com>
2023-11-27 14:22:40 +0530

    Updated Services

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Appointment from './pages/Appointment';
-import PetsBuy from './pages/PetsBuy';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Appointment from "./pages/Appointment";
+import PetsBuy from "./pages/PetsBuy";
+import { Route, Routes } from "react-router-dom";
+import data from "./data.json";
+import Services from "./pages/Services";
 
 function App() {
-  return (
-    <div >
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/appointment' element={<Appointment/>}/>
-        <Route path='/petsbuy' element={<PetsBuy/>}/>
-      </Routes>
-      <Footer/>
-    </div>
-  );
+    const data1 = data.vaccine;
+    console.log(data1);
+
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/appointment" element={<Appointment />} />
+
+                <Route path="/vaccination" element={<Services details = "vaccination" />} />
+                <Route path="/wellness" element={<Services details = "wellness" />} />
+                <Route path="/dental" element={<Services details = "dental" />} />
+                
+                <Route path="/petsbuy" element={<PetsBuy />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
